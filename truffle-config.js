b2474ce99d5becbd68c4087369d298e4bdf8c146
TR <cragtlab@gmail.com>
2023-09-05 16:47:38 +0800

    test

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
 	networks: {
 		development: {
 			host: "127.0.0.1",
-			port: 8545,
+			port: 7545,
 			network_id: "*" // Match any network id
-		}
+		}, // https://chainlist.org/chain/80001
+		matic: {
+			provider: () => new HDWalletProvider("722db7152fc524a2aea25f239db43c75ea3f132acb145036a5e523bbbf8070eb", `https://rpc-mumbai.maticvigil.com`),
+			network_id: 80001,
+			confirmations: 2,
+			timeoutBlocks: 200,
+			skipDryRun: true,
+			networkCheckTimeout: 10000,
+			gasPrice: 45000000000,
+
+		},
 	},
 
 	contracts_directory: './src/contracts/',
 	contracts_build_directory: './src/abis/',
-
+	plugins: ['truffle-plugin-verify'],
+	api_keys: {
+	  polygonscan: '5PB37NJQ9BMMMWCA6TTSRPSTXJJWNATMIM'
+	},
 	compilers: {
 		solc: {
 			version: '0.8.9',
